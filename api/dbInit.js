76a2803f2b7db19c37f1b2933908d34c9da90663
wupeng <wupeng@uctimes.cn>
2017-09-08 09:36:14 +0800

    pull mysql文件配置

--- api/dbInit.js
+++ api/dbInit.js
@@ -10,7 +10,7 @@ var dbInit = function () {
       sql.query('CREATE TABLE person(userId smallint(6) unsigned NOT NULL auto_increment,user varchar(255),password varchar(255),name varchar(255),visitNum int(10),PRIMARY KEY (`userId`)) ENGINE=InnoDB DEFAULT CHARSET=gbk', function (err) {
         !err ? console.log('person表创建成功') : console.log(err)
       })
-      sql.query('CREATE TABLE blog(blogId smallint(6) unsigned NOT NULL auto_increment,id varchar(255),name varchar(255),type varchar(255),text longtext,md longtext,comment varchar(255),star int(10),PRIMARY KEY (`blogId`) ) ENGINE=InnoDB DEFAULT CHARSET=gbk', function (err) {
+      sql.query('CREATE TABLE blog(blogId smallint(6) unsigned NOT NULL auto_increment,id varchar(255),name varchar(255),type varchar(255),text longtext,md longtext,comment varchar(255),star int(10),time varchar(255),PRIMARY KEY (`blogId`) ) ENGINE=InnoDB DEFAULT CHARSET=gbk', function (err) {
         !err ? console.log('blog表创建成功') : console.log(err)
       })
       sql.query('CREATE TABLE comment(commentId smallint(6) unsigned NOT NULL auto_increment,id varchar(255) ,blogId varchar(255),text varchar(255),PRIMARY KEY (`commentId`)) ENGINE=InnoDB DEFAULT CHARSET=gbk', function (err) {
